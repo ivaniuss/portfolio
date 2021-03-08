afb9d0bb0bd60885ce1cacacd26f965c8383af26
ivaniuss <ivan.emi94@gmail.com>
2021-03-08 00:33:05 -0500

    correo

---
+++ correo.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['submit'])){
+    if (!empty($_POST['name']) && !empty($_POST['subject']) 
+        && !empty($_POST['email']) && !empty($_POST['message'])){
+            $name = $_POST['name'];
+            $msg = $_POST['message'];
+            $email = $_POST['email'];
+            $asunto = $_POST['subject'];
+            $header = "From: noreply@example.com" . "\r\n";
+            $header = "Reply-To:ivan.emi94@gmail.com" . "\r\n";
+            $header = "X-Mailer: PHP/" . phpversion();
+            $mail = @mail($email,@$asunto,@msg,@header);
+            if($mail){
+                echo "<h4> !Mail enviado exitosamente!<h4>";
+            }
+        }
+}
+
+?>
